db01d5eb9fca9a135c4686a147c6d65ab6deb0c9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-24 21:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ function distanceTravelledInFeet(start, finish) {
 
 function calculatesFarePrice(start, finish) {
   const feet = distanceTravelledInFeet(start, finish)
-  if feet < 400 {
-    let fare = 0
-  } else if feet > 2500 {
-    let fare = 'cannot travel that far'
-  }
-  
+  if (feet < 400) {
+    fare = 0
+  } else if (feet > 2500) {
+    fare = 'cannot travel that far'
+  } else if (feet > 2000) {
+    fare = 25
+  } else if {
+    fare = 0.02 * feet
+  }    
 }
